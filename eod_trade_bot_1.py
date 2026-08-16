20826fdb82b38f2954a8890c97b55ab3e2de8ae8
ashky11 <106438666+ashky11@users.noreply.github.com>
2026-08-16 21:26:41 +0530

    Friday_Test:Update model version and fix print statement
    This is the code change just to test if we get breakouts for the Firdays session clousure. model version change

--- eod_trade_bot_1.py
+++ eod_trade_bot_1.py
@@ -46,7 +46,7 @@ Strictly return ONLY valid JSON matching this schema with no markdown surroundin
 }}
 """
         response = client.models.generate_content(
-            model="gemini-2.5-flash-latest",
+            model="gemini-2.0-flash",
             contents=prompt
         )
         
